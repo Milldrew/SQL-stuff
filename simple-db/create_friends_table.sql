4ede39e6fd699d824416870206b156919b8ed55a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-14 09:51:04 -0600

    📅  | Mon Mar 14 09:51:04 MDT 2022

---
+++ simple-db/create_friends_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE friends (
+  id SERIAL,
+  name TEXT,
+  birthday DATE
+);
---
+++ simple-db/insert_friends.sql
@@ -0,0 +1,4 @@
+INSERT INTO friends (name, birthday)
+VALUES ('John Doe', 'December 12, 2012' );
+
+SELECT * FROM friends;
